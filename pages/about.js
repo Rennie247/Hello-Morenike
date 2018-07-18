3fdc7ebb053f7f464839649c3f0097398469f8d1
Adetola Adeoye <adeoyeadetola@gmail.com>
2018-07-18 09:23:11 -0400

    adding psges yo!

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+// pages/about.js
+
+import Layout from '../components/MyLayout.js'
+
+export default () => (
+    <Layout>
+       <p>This is the About page</p>
+       <p>Fashion and Style</p>
+       <p>Nigerian Ankara</p>
+       <p>Vityusha Trends</p>
+       <p> Contact Us</p>
+    </Layout>
+)
